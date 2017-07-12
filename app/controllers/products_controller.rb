921b53644d9ff947d587c975fbd480c2a98b581f
dlifton <dlifton@uchicago.edu>
2017-07-12 15:46:59 -0500

    hw3 submission

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,43 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find_by(id: params["id"])
+  end
+
+  def new
+  end
+
+  def create
+    p = Product.new
+    p.title = params["title"]
+    p.price = params["price"].to_f * 100
+    p.description = params["description"]
+    p.save
+
+    redirect_to "/products"
+  end
+
+  def edit
+    @product = Product.find_by(id: params["id"])
+  end
+  
+  def patch
+    p = Product.find_by(id: params["id"])
+    p.title = params["title"]
+    p.price = params["price"].to_f * 100
+    p.description = params["description"]
+    p.save
+    redirect_to "/products/#{params['id']}"
+  end
+
+  def destroy
+    p = Product.find_by(id: params["id"])
+    p.delete
+    redirect_to "/products"
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+
+  root 'products#index'
+
+  get '/products' => 'products#index'
+  post '/products' => 'products#create'
+
+  get '/products/new' => 'products#new'
+
+  get '/products/:id' => 'products#show'
+
+  get '/products/:id/edit' => 'products#edit'
+  patch '/products/:id' => 'products#patch'
+
+  delete '/products/:id' => 'products#destroy'
+
+  get '/help' => 'user_stories#index'
+
+end
